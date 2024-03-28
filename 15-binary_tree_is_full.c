d7b469c1fce4deb0658a73d9a7bfcb85070bd168
Banjamin Torfu <btorfu@proton.me>
2024-03-28 05:54:14 +0000

    full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,14 +9,15 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int full;
-
 	if (tree == NULL)
 		return (0);
 
 	if ((tree->left == NULL) && (tree->right == NULL))
 		return (1);
-	else
+
+	if (((tree->left ==NULL) && (tree->right != NULL)) || 
+		((tree->left !=NULL) && (tree->right == NULL)))
 		return (0);
-	return (((binary_tree_is_full(tree->left)  && ()) || () && ());
+
+	return ((binary_tree_is_full(tree->left))  && (binary_tree_is_full(tree->right)));
 }
